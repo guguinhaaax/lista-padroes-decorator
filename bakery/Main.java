0e47b1215d85bf9d26cafdc1d6ea89991b0ecca7
guguinhaaax <gustavo.melo.fla@gmail.com>
2025-10-08 05:28:42 -0300

    feat: atualizar Main para os novos modelos de bolo

--- bakery/Main.java
+++ bakery/Main.java
@@ -1,3 +1,4 @@
+// Main.java
 /**
  * Create an order and print it
  */
@@ -5,10 +6,28 @@ public class Main {
     public static void main(String[] args) {
         // Create the order
         Order order = new Order();
-        order.addCake(new VanillaCake());
+
+        // Bolo de chocolate
         order.addCake(new ChocolateCake());
 
+        // Bolo de baunilha com o dizer "PLAIN!"
+        order.addCake(new WithSaying(new VanillaCake(), "PLAIN!"));
+
+        // Bolo de baunilha com granulado com os dizeres "FANCY"
+        order.addCake(new WithSaying(new WithSprinkles(new VanillaCake()), "FANCY!"));
+
+        // Bolo de morango em várias camadas com granulado duplo e dois dizeres
+        Cake strawberrySpecial = new StrawberryCake();
+        strawberrySpecial = new MultiLayered(strawberrySpecial);
+        strawberrySpecial = new WithSprinkles(strawberrySpecial);
+        strawberrySpecial = new WithSprinkles(strawberrySpecial); // Segundo granulado
+        strawberrySpecial = new WithSaying(strawberrySpecial, "One of");
+        strawberrySpecial = new WithSaying(strawberrySpecial, "EVERYTHING");
+        order.addCake(strawberrySpecial);
+
+
         // Print the order
+        System.out.println("Order Details:");
         order.printOrder();
     }
 }
